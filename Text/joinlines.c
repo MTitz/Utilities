91a23248232cbeb66ab6e88ddc39c7150952fc24
Martin Titz <martin.titz@gmail.com>
2026-01-05 08:24:04 +0100

    Update joinlines.c to newer version

--- Text/joinlines.c
+++ Text/joinlines.c
@@ -1,6 +1,6 @@
 /* File: joinlines.c */
 
-/* Version 0.1.1, Martin Titz, 2013 */
+/* Version 0.1.5, Martin Titz, 2013, 2014, 2016 */
 
 #include <ctype.h>
 #include <errno.h>
@@ -9,12 +9,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 16777216
 #endif
 
+static int addPagebreaks = 0;
+static int ignoreEmptyLines = 0;
+static int minLineLength = 0;
+static int maxNewlines = 0;
 static int verbose = 0;
 static int error_count = 0;
+static int experimentalMode = 0;
 static unsigned char buffer[BUFFER_SIZE];
 #ifdef __unix__
 static char *progname;
@@ -24,27 +32,38 @@ static const char *const progname = "joinlines";
 
 static void usage(void)
 {
-    fprintf(stderr, "usage: %s [-v] [file]...\n", progname);
+    fprintf(stderr, "usage: %s [-E] [-l minLineLength] [-m maxNewlines] [-p] [-v] [file]...\n", progname);
+    fprintf(stderr, "  -E   ignore empty lines\n"
+                    "  -l   set minmal line length for joining\n"
+                    "  -p   add pagebreaks\n"
+                    "  -e   experimental joining of pdf2text files\n"
+        );
 }
 
-#ifdef UNUSED
 static void arg_err(char c)
 {
     fprintf(stderr, "%s: option -%c requires an argument.\n", progname, c);
     usage();
     exit(EXIT_FAILURE);
 }
-#endif
 
+static int noLinefeedExpected(unsigned char lastCh)
+{
+    return isalnum(lastCh) || lastCh == ',' || lastCh == ';' || lastCh == '%';
+}
 
-static void join_lines(FILE *f, size_t n)
+static void join_lines(FILE *file, size_t n)
 {
     size_t i;
     int lfCount = 0;
     int crCount = 0;
     int nlCount = 0;
+    int lastLineLength = 0;
+    unsigned char lastCh = '\0';  /* only for experimental mode */
     for (i = 0; i < n; ++i) {
         unsigned char ch = buffer[i];
+        if (verbose >= 2)
+            fprintf(stderr, " processing character %d\n", ch);
         if (ch == '\n') {
             ++lfCount;
         } else if (ch == '\r') {
@@ -60,37 +79,66 @@ static void join_lines(FILE *f, size_t n)
                 nlCount = 0;
             }
             if (nlCount > 0) {
-                if (nlCount == 1) {
-                    fputc(' ', f);
+                /* Here one or more newlines and a first character afterwards had been read in. */
+                if (verbose >= 2)
+                    fprintf(stderr, "lastLineLength = %d\n", lastLineLength);
+                if (nlCount <= 1 + ignoreEmptyLines && lastLineLength >= minLineLength && (!experimentalMode || noLinefeedExpected(lastCh))) {
+                    /* Join lines. */
+                    if (ch != ' ')
+                        fputc(' ', file);
                 } else {
+                    /* Keep lines separate by emitting newlines in Unix style. */
                     int j;
+                    if (maxNewlines > 0 && nlCount > maxNewlines) {
+                        nlCount = maxNewlines;
+                    }
                     for (j = 0; j < nlCount; ++j) {
-                        fputc('\n', f);
+                        fputc('\n', file);
+                    }
+                    for (j = 0; j < addPagebreaks; ++j) {
+                        fputc('\n', file);
                     }
                 }
                 lfCount = 0;
                 crCount = 0;
                 nlCount = 0;
+                lastLineLength = 1; /* We already have read the first character of this line. */
+            } else {
+                ++lastLineLength;
             }
-            fputc(ch, f);
+            fputc(ch, file);
+
+            /* Set lastCh to last 'relevant' character, here ch cannot be LF or CR */
+            if (ch != ' ' && ch != '\t') {
+                lastCh = ch;
+            }
         }
+    }
+    fputc('\n', file);
+    int fileNo = fileno(file);
+    if (fileNo > 0 && fileNo != fileno(stdout) && ftruncate(fileNo, ftell(file)) != 0) {
+        fprintf(stderr, "%s: ftruncate error (%s)\n", progname, strerror(errno));
     }
 }
 
-static void do_handle(FILE *f)
+static void do_handle(FILE *file)
 {
-    size_t bytes_read = fread(buffer, 1, BUFFER_SIZE, f);
-    if (ferror(f)) {
+    size_t bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
+    if (ferror(file)) {
         fprintf(stderr, "%s: read error (%s)\n", progname, strerror(errno));
         ++error_count;
-    } else if (feof(f)) {
-        /* Process bytes_read bytes stored in the buffer */
-        fseek(f, 0L, SEEK_SET);
-        if (ferror(f)) {
-            fprintf(stderr, "%s: seek error (%s)\n", progname, strerror(errno));
-            ++error_count;
+    } else if (feof(file)) {
+        if (file != stdin) {
+            /* Process bytes_read bytes stored in the buffer */
+            fseek(file, 0L, SEEK_SET);
+            if (ferror(file)) {
+                fprintf(stderr, "%s: seek error (%s)\n", progname, strerror(errno));
+                ++error_count;
+            } else {
+                join_lines(file, bytes_read);
+            }
         } else {
-            join_lines(f, bytes_read);
+            join_lines(stdout, bytes_read);
         }
     } else {
         fprintf(stderr, "%s: could not read until end of file.\n", progname);
@@ -100,9 +148,9 @@ static void do_handle(FILE *f)
 
 static void do_file(const char *fname)
 {
-    FILE *f;
+    FILE *file;
 
-    if ((f = fopen(fname, "rb+")) == 0) {
+    if ((file = fopen(fname, "rb+")) == 0) {
         fprintf(stderr, "%s: can't open %s (%s)\n",
                 progname, fname, strerror(errno));
         ++error_count;
@@ -111,8 +159,8 @@ static void do_file(const char *fname)
     if (verbose) {
         printf("\nFile %s:\n\n", fname);
     }
-    do_handle(f);
-    fclose(f);
+    do_handle(file);
+    fclose(file);
 }
 
 int main(int argc, char *argv[])
@@ -138,10 +186,31 @@ int main(int argc, char *argv[])
             }
             while (*++*argv) {
                 switch (**argv) {
+                    case 'e':
+                        experimentalMode = 1;
+                        break;
+                    case 'E':
+                        ++ignoreEmptyLines;
+                        break;
                     case 'h':
                     case '?':
                         usage();
                         return EXIT_SUCCESS;
+                    case 'l':
+                        if (*++*argv || --argc && *++argv) {
+                            minLineLength = atoi(*argv);
+                        } else
+                            arg_err('l');
+                        goto nextarg;
+                    case 'm':
+                        if (*++*argv || --argc && *++argv) {
+                            maxNewlines = atoi(*argv);
+                        } else
+                            arg_err('m');
+                        goto nextarg;
+                    case 'p':
+                        ++addPagebreaks;
+                        break;
                     case 'v':
                         ++verbose;
                         break;
@@ -161,9 +230,7 @@ int main(int argc, char *argv[])
             do_file(*argv);
             ++files_done;
         }
-#ifdef UNUSED
         nextarg: ;
-#endif
     }
     if (!files_done)
         do_handle(stdin);
